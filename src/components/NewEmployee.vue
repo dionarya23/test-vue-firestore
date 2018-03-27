ae4c28ad1701cea9b48ec7db5996e49e1e8a229e
Dion Arya Pamungkas <dionarya.p@gmail.com>
2018-03-27 22:13:23 +0700

    I :heart: You :couple:

---
+++ src/components/NewEmployee.vue
@@ -0,0 +1,70 @@
+<template>
+  <div id="new-employee">
+    <div class="row">
+      <form @submit.prevent="saveEmployee" class="col s12">
+        
+        <div class="row">
+          <div class="field col s12">
+            <input type="text" v-model="employee_id" required>
+            <label>Employee ID#</label>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="field col s12">
+            <input type="text" v-model="name" required>
+            <label>Name</label>
+          </div>
+        </div>
+
+        <div class="row">
+          <div class="field col s12">
+            <input type="text" v-model="dept" required>
+            <label>Deparment</label>
+          </div>
+        </div>
+
+             <div class="row">
+          <div class="field col s12">
+            <input type="text" v-model="position" required>
+            <label>Potision</label>
+          </div>
+        </div>
+  <button class="btn" type="submit">submit</button>
+     <router-link to="/" class="btn grey">Cancel</router-link>
+      </form>
+    </div>
+
+    </div>
+</template>
+
+<script>
+import db from './firebaseInit'
+export default {
+  name : 'new-employee',
+  data () {
+      return {
+          employee_id: null,
+          name: null,
+          dept: null,
+          position: null
+      }
+  },
+  methods: {
+    saveEmployee () {
+      db.collection('employees').add({
+        employee_id: this.employee_id,
+        name: this.name,
+        dept: this.dept,
+        position: this.position
+      }).then(docRef => {
+        this.$router.push('/')
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+  }
+}
+</script>
+
---
+++ src/components/firebaseConfig.js
@@ -0,0 +1,8 @@
+export default {
+    apiKey: "AIzaSyBkBsfszFsPS50-fSvqUirr0VKUJPoeQo4",
+    authDomain: "test-b07d9.firebaseapp.com",
+    databaseURL: "https://test-b07d9.firebaseio.com",
+    projectId: "test-b07d9",
+    storageBucket: "test-b07d9.appspot.com",
+    messagingSenderId: "469006864956"
+}
